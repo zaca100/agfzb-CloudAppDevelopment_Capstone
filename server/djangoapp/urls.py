d23bb8259feab1ca5fabe33e6e257bda908abd58
Flavio <zaca10@gmail.com>
2022-03-25 21:35:48 +0000

    update3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from . import views 
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,17 +10,17 @@ urlpatterns = [
     # name the URL
     path(route='', view=views.index, name='index'),
     # path for about view
-    path(route='', view=views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
-    path(route='', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
-    path(route='', view=views.registration_request, name='register'),
+    path(route='register', view=views.registration_request, name='register'),
     # path for login
-    path(route='', view=views.login_request, name='login'),
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
-    path(route='', view=views.logout_request, name='logout'),
+    path(route='logout', view=views.logout_request, name='logout'),
     # path for dealerships
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='get_dealerships', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
     path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
